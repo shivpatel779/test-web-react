c68bdceabe80e3bdb44fade9c8438f0a72a31d48
shiv patel <shivpatel@gmail.com>
2020-04-21 15:58:26 +0530

    featuer fixed

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light navbar-laravel">
         <div className="container">
-          <a className="navbar-brand" href="#">Laravel</a>
+          <a className="navbar-brand" href="#">Test App</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
           </button>
